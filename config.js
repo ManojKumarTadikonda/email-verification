4c5b0269d20c2f4447f5449a0a18eac7e9f438b1
ManojKumarTadikonda <manojtadikonda5@gmail.com>
2024-12-30 14:27:48 +0530

    db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/auth-db",
+    MONGO_URI: process.env.MONGO_URI || "mongodb+srv://manojtadikonda5:manoj38519@cluster0.8lhda.mongodb.net/auth-db",
     JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASS: process.env.EMAIL_PASS,
